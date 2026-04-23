279057705493789deb0f5cc675c35402b25ffe65
AOAO62 <asami071907190719@gmail.com>
2026-04-23 13:19:35 +0900

    Fix import statement for routes in App.tsx
    Updated import statement to explicitly include .tsx extension for routes.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,7 @@
 import { RouterProvider } from "react-router";
-import { router } from "./routes";
+// もし import { ... } from './routes' となっていたら
+// 以下のように .tsx を明示的に付けるか、パスを確認します
+import { routes } from './routes.tsx';
 
 export default function App() {
   return <RouterProvider router={router} />;
