76d4e10fac75d424b032b90431fe7118d758083b
AOAO62 <asami071907190719@gmail.com>
2026-04-23 11:53:13 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // ↓ この一行を追加してください（URLの末尾に合わせる設定です）
   base: '/keiri-note/',
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: [
-        'figma:foundry-client-api'
-      ]
+      external: ['figma:foundry-client-api']
     }
+  },
+  // ↓ これを追加：プログラムの中で使われていてもエラーにしない設定
+  define: {
+    'process.env': {}
   }
 })
