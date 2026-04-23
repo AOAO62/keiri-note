63bd7c3b1e6a42c5b6817a38412568f28c088e92
AOAO62 <asami071907190719@gmail.com>
2026-04-23 11:44:14 +0900

    Set base URL for deployment to '/keiri-note/'
    Added base URL configuration for deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // ↓ この一行を追加してください（URLの末尾に合わせる設定です）
+  base: '/keiri-note/',
   plugins: [react()],
   build: {
     rollupOptions: {
-      // ここが重要：見つからない部品を「外部のもの」として扱う設定です
       external: [
         'figma:foundry-client-api'
       ]
